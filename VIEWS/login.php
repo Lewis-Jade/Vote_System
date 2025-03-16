e6171564a9acb18f080b77568c86606192ef98be
Lewis <mudaidalewis@gmail.com>
2025-03-16 15:55:44 +0300

    Added a login file

---
+++ VIEWS/login.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="../CSS/login.css">
+</head>
+<body>
+
+<section>
+   <div class="container">
+       <div class="box">
+       <div class="front">
+        <form action="">
+
+        <h2>Login</h2>
+            <div class="inputbox">
+                <input type="email" placeholder="email" required>
+                <span></span>
+            </div>
+            <div class="inputbox">
+                <input type="password" placeholder="password" required>
+                <span></span>
+            </div>
+            <div class="inputbox">
+                <p>Don't have an Account?</p>   
+                 <button>Sign Up</button>
+            </div>
+            
+            <input type="submit"  value="Login">
+            
+        </form>
+       </div>
+       <div class="back">back</div>
+       </div>
+   </div>
+</section>
+    
+</body>
+</html>
