f37e4038434b1c4cff6966214079f3606519380a
Lewis <mudaidalewis@gmail.com>
2025-03-17 13:47:27 +0300

    Almost there

---
+++ SCRIPTS/index.js
@@ -0,0 +1,24 @@
+
+const getButton = document.getElementById('btn');
+
+const   cardBox = document.getElementById('rotateCard');
+const signBtn = document.getElementById('btn-signup');
+
+const section = document.getElementById('section')
+
+
+getButton.addEventListener("click",  function(){
+   
+    cardBox.classList.add('rotate');
+    section.style.backgroundColor = "rgba(233, 67, 17, 0.62)";
+
+});
+
+
+signBtn.onclick = function(){
+
+       cardBox.classList.remove('rotate');
+       section.style.backgroundColor = "";
+
+
+}
--- VIEWS/login.php
+++ VIEWS/login.php
@@ -5,12 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
     <link rel="stylesheet" href="../CSS/login.css">
+    <script src="../SCRIPTS/index.js" defer></script>
 </head>
 <body>
 
-<section>
+<section    id="section">
    <div class="container">
-       <div class="box">
+       <div class="box"  id="rotateCard">
        <div class="front">
         <form action="">
 
@@ -25,17 +26,50 @@
             </div>
             <div class="inputbox">
                 <p>Don't have an Account?</p>   
-                 <button>Sign Up</button>
+                 <button  id="btn">Sign Up</button>
             </div>
             
             <input type="submit"  value="Login">
             
         </form>
        </div>
-       <div class="back">back</div>
+       <div class="back">
+       <form action=""  class="sign-up">
+
+        <h2>Sign Up</h2>
+            <div class="inputbox">
+                <input type="text" placeholder="firstname" required>
+                <span></span>
+            </div>
+            <div class="inputbox">
+                <input type="text" placeholder="lastname" required>
+                <span></span>
+            </div>
+            <div class="inputbox">
+                <input type="email" placeholder="email" required>
+                <span></span>
+            </div>
+            <div class="inputbox">
+                <input type="password" placeholder="password" required>
+                <span></span>
+            </div>
+            <div class="inputbox">
+                <input type="password" placeholder="confirm password" required>
+                <span></span>
+            </div>
+            <div class="inputbox">
+                <p>Already have an Account?</p>   
+                <button  id="btn-signup">Login</button>
+            </div>
+            
+            <input type="submit"  value="Sign Up">
+    
+</form>
+       </div>
        </div>
    </div>
 </section>
     
 </body>
+
 </html>
