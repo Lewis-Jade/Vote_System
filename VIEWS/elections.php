861e533a4d1c489c057ea119b33fd0af1a92563e
Lewis <mudaidalewis@gmail.com>
2025-04-10 13:16:23 +0300

    hshshshs

---
+++ VIEWS/elections.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Availabe Elections</title>
+    <link rel="stylesheet" href="../CSS/elections.css">
+</head>
+<body>
+    
+    <h1>Available Elections</h1>
+</body>
+</html>
