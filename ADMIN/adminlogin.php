8a438a5f15922645707d708910f3b1ad9ec9bc25
Lewis <mudaidalewis@gmail.com>
2025-04-11 13:07:04 +0300

    added an admin pannel

---
+++ ADMIN/adminlogin.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>admin login</title>
+    <link rel="stylesheet" href="../CSS/adminlogin.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+<body>
+    <section>
+        <div class="info">
+        <i class="fas fa-vote-yea"></i>
+          <h1>VoteExpress</h1>
+        
+        </div>
+        <form action="">
+    <h2>Login</h2>
+            <div class="inputbox">
+                <input type="email" placeholder="email"  name="email" required>
+                <span></span>
+            </div>
+            <div class="inputbox">
+                <img  id="eye-login"  src="../IMG/eyebrow.png">
+                <input type="password" placeholder="password"  name="password"  id="login-password" autocomplete="new-password"  required>
+                <span></span>
+            </div>
+           
+          
+            <input type="submit"  name="submit" value="Login">
+
+        </form>
+    </section>
+</body>
+</html>
+
+
