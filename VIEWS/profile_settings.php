6dbf9fdd2f3882d1ced66e5efa4c89ba6c51ed97
Lewis <mudaidalewis@gmail.com>
2025-04-17 12:57:30 +0300

    The mad bad man

--- VIEWS/profile_settings.php
+++ VIEWS/profile_settings.php
@@ -1,1 +1,20 @@
-s
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Profile settings</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="../CSS/profile.css">
+</head>
+<body>
+<a href="dashboard.php"><i class="fa-solid fa-arrow-left"></i></a>
+<form action="">
+       <h1>Update Personal Information</h1>
+      <input type="text"  placeholder="Firstname" required>
+      <input type="text"  placeholder="Lastname" required>
+      <input type="email"   placeholder="email" required>
+      <input type="submit"  value="Update">
+</form>
+</body>
+</html>
